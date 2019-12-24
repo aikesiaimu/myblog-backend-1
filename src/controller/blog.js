500bdbbd447b886212392c3e9677316c6cead248
hitryan <aikesiaimu@outlook.com>
2019-12-24 09:53:37 +0800

    4-8

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -3,21 +3,31 @@ const getList = (author, keyword) => {
     return [
         {
             id: 1,
-            titile: '标题A',
+            title: '标题A',
             content: '内容A',
             createTime: 1577146805170,
             author: 'Ryan'
         },
         {
             id: 2,
-            titile: '标题B',
+            title: '标题B',
             content: '内容B',
             createTime: 1577146876883,
             author: 'Holly'
         }
     ]
 }
-
+const getDetail = (id) => {
+    //先返回格式正确的假数据
+    return {
+        id: 1,
+        title: '标题A',
+        content: '内容A',
+        createTime: 1577146805170,
+        author: 'Ryan'
+    }
+}
 module.exports = {
-    getList
+    getList,
+    getDetail
 }
